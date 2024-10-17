a6c93f8a8c64ed855e9266eec97b24a9bd64ac6e
bluerune <b.sofia.sosa@gmail.com>
2024-10-17 08:27:28 -0300

    CSS

--- js/space.js
+++ js/space.js
@@ -7,51 +7,49 @@ function busqueda(){
     url = "https://images-api.nasa.gov/search?q=" + (inputBuscar.toLowerCase()) ;
 
     //Loading
-    contenedor.innerHTML = `<p>Loading</p>`;
+    contenedor.innerHTML = `<img src="img/icons_loading.png" id="loading" class="rotate-circle" alt="Cargando">`;
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            
-            console.log(data);
             //desestructurar data
-           
             const { items } = data.collection;
+            if (items.length <= 0){
+                contenedor.innerHTML = `<p id="notfound">No se encontró nada que coincida con tu búsqueda.</p>`;
+            } else {
+                //limpiar contenedor
+                contenedor.innerHTML = ``;
 
-            console.log(items);
-            //const { data: dataItems, links } = items;
-            
-           
-            //const { date_created, description, title} = dataItems;
-            
-
-            //limpiar contenedor
-            contenedor.innerHTML = ``;
-
-            //crear tarjetas
-            items.forEach(element => {
-
-                console.log(element);
-                const { data: dataItem, links } = element;
-                
-                contenedor.innerHTML += `<div class="card" style="width: 18rem;">
-                    <img src="${ links[0].href }" class="card-img-top" alt="${ dataItem[0].title }">
-                    <div class="card-body">
-                        <h5 class="card-title">${ dataItem[0].title }</h5>
-                        <p class="card-text">${ dataItem[0].description }</p>
-                        <p class="text-muted">${ dataItem[0].date_created }</p>
-                    </div>
-                    </div>`;
-
-            });
-
+                //crear tarjetas
+                items.forEach(element => {
+                    console.log(element);
+                    const { data: dataItem, links, href } = element;
+                    if (!links) {
+                        imgUrl = "img/icons_backgrounds.png";
+                    } else {
+                        imgUrl = links[0].href;
+                    }
+                    contenedor.innerHTML += `<div class="card">
+                        <img src="${ imgUrl }" class="card-img-top" alt="${ dataItem[0].title }">
+                        <div class="card-body">
+                            <h5 class="card-title">${ dataItem[0].title }</h5>
+                            <p class="card-text">${ dataItem[0].description }</p>
+                            <p class="text-muted">${ dataItem[0].date_created }</p>
+                        </div>
+                        </div>`;
+                });
+                }
         })
         .catch(error => {
-            
-            console.error(error);
+            console.error("Error al buscar imagenes", error);
         })
 
 }
 
-
 btnBuscar.addEventListener("click", busqueda);
+
+inputBuscar.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        btnBuscar.click();
+    }
+});
